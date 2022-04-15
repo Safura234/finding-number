b73e34e44eb533c48d93c5f8cc844d2c59ae72e5
Safura234 <safuramg@code.edu.az>
2022-04-15 17:15:42 +0400

    https://github.com/Safura234/finding-number.git

---
+++ finding number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace finding_number
+{
+    internal class Program
+    {
+        private static object array;
+        private static int length;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Finding number");
+            int num = 468;
+           while (true)
+            { 
+                if (num % 10 == 8)
+                {
+                    int result = num % 10;
+                    Console.WriteLine(result);
+                    return;
+                }
+                num = num / 10; 
+            }
+        
+            }
+        }
+    }
+
